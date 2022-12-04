d322a129e06e2ceb90e4d9df57bf664a80376f3f
reza0310 <michelageou@gmail.com>
2022-12-04 03:14:29 +0100

    Patch rapide affichage incomplet

--- liste/index.php
+++ liste/index.php
@@ -3,6 +3,9 @@ set_include_path($_SERVER['DOCUMENT_ROOT']."/wishlist");
 include 'mdp.php';
 include 'utils.php';
 
+ini_set('display_error', 1);
+mysqli_report(MYSQLI_REPORT_ALL);
+
 $nom = $_GET["nom"];
 
 $con=mysqli_connect($servername,$username,$password,$dbname);
@@ -13,31 +16,33 @@ if (mysqli_connect_errno()) {
 
 $page = "";
 $priorites = array('HAUTE', 'MOYENNE PLUS', 'MOYENNE MOINS', 'BASSE');
+$query = $con->prepare("SELECT * FROM voeux WHERE proprietaire=?");
+$query->bind_param("s", $nom);
+$query->execute();
+$result = mysqli_fetch_all($query->get_result());
+$query->close();
+
 foreach ($priorites as $pri) {
-	$query = $con->prepare("SELECT * FROM voeux WHERE proprietaire=? AND priorite=?");
-	$query->bind_param("ss", $nom, $pri);
-	$query->execute();
-	$result = $query->get_result();
-	$query->close();
-	
 	$page .= "<h1 class='categories'>Priorité ".strtolower($pri).":</h1>";
-	while ($colonne = mysqli_fetch_array($result)) {
-		if ($colonne[5] != 1) {
-			$nom = strval($colonne[5]) . " &times " . $colonne[1];
-		} else {
-			$nom = $colonne[1];
+	foreach ($result as $colonne) {
+		if ($colonne[6] == $pri) {
+			if ($colonne[5] != 1) {
+				$nom = strval($colonne[5]) . " &times " . $colonne[1];
+			} else {
+				$nom = $colonne[1];
+			}
+			if ($colonne[3] == NULL) {
+				$image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Question_mark_alternate.svg/1200px-Question_mark_alternate.svg.png";
+			} else {
+				$image = $colonne[3];
+			}
+			$page .= "<div class='ticket'>";
+			$page .= "<a href='".clearhtml($colonne[2])."'>";
+			$page .= "<img src='".clearhtml($image)."' alt='L image du voeux' class='image_ticket'>";
+			$page .= "<div class='texte_ticket'>".clearhtml($nom)."<br>";
+			$page .= clearhtml($colonne[4])."€";
+			$page .= "</div></a></div>";
 		}
-		if ($colonne[3] == NULL) {
-			$image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Question_mark_alternate.svg/1200px-Question_mark_alternate.svg.png";
-		} else {
-			$image = $colonne[3];
-		}
-		$page .= "<div class='ticket'>";
-		$page .= "<a href='".clearhtml($colonne[2])."'>";
-		$page .= "<img src='".clearhtml($image)."' alt='L image du voeux' class='image_ticket'>";
-		$page .= "<div class='texte_ticket'>".clearhtml($nom)."<br>";
-		$page .= clearhtml($colonne[4])."€";
-		$page .= "</div></a></div>";
 	}
 }
 

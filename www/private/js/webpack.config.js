a7b1fa8a16bae4ccceb9293195c3fdf0a1231b0e
Celian A <celian.aldehuelo@gmail.com>
2024-01-03 15:13:35 +0100

    Update fiel hierarchy again and have a working TypeScript and webpack setup for web page

--- www/private/js/webpack.config.js
+++ www/private/js/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     devtool: 'inline-source-map',
     mode: 'development',
     devServer: {
-        static: ['./dist', './public', './assets'],
+        static: ['./dist', '../html/dist', '../assets'],
         client: {
             progress: true,
         },

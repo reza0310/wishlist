b5d75f14f9a568345b242794379bcde651f6ffb8
Celian A <celian.aldehuelo@gmail.com>
2024-01-22 16:40:36 +0100

    Working webpage preprocessing flow and Typescript compilation

--- web/www/build.bat
+++ web/www/build.bat
@@ -7,6 +7,7 @@ IF "%~1"=="--watch" (
     python "%PREPROCESSOR%" private_html/ public/ --list pages.lst --override --watch
     rem taskkill /fi "WindowTitle eq webpack_dev_server"
     wmic process where "name like '%%node.exe%%' and commandline like '%%run serve%%'" delete
+    exit
 ) else (
     python "%PREPROCESSOR%" private_html/ public/ --list pages.lst --override
     npm run build
